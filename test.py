f304f62b82325e3b830326d9a4753de50542be2d
Jon Gong <urmotherinlaw69@gmail.com>
2018-12-19 00:54:51 -0600

    Revert "deleted everything"
    This reverts commit 87496bcfbc3de88921084d5ed61ea7dd50724a32.

---
+++ test.py
@@ -0,0 +1,11 @@
+# Initial file for testing purposes
+
+def fibonacci(n):
+	if ((n == 0) | (n == 1)):
+		return n
+	return (fibonacci(n-1) + fibonacci(n-2))
+
+print("Hello world!")
+print(fibonacci(6))
+print("ligma balls")
+print("deal with sughandese")
